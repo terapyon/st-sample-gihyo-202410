370dd3bed0cf1e0cd7f0335c7cddb2eb84597a5c
Manabu TERADA <terada@cmscom.jp>
2024-10-19 13:15:11 +0900

    表示を修正

--- choice_app.py
+++ choice_app.py
@@ -2,9 +2,9 @@ import random
 import streamlit as st
 
 st.title("文字列を選択するアプリ")
-text = st.text_input("スペース区切りで単語を入力してください。")
-splited_text = text.replace("　", " ").split(" ")
-st.write("入力した選択肢: ", splited_text)
-if st.button("選択肢を表示"):
-    choice = random.choice(splited_text)
-    st.write("選択した選択肢: ", choice)
+text = st.text_input("スペース区切りで単語を入力してください。")  # 入力ボックス
+splited_text = text.replace("　", " ").split(" ")  # スペース区切りでリスト化
+st.write("入力した単語: ", splited_text)  # リストを表示
+if st.button("一つを選ぶ"):  # ボタンを設置し、ボタンが押されたら実行
+    choice = random.choice(splited_text)  # ランダムに1つだけを選択
+    st.write("選択された単語: ", choice)  # 選択されたものを表示
