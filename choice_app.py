c96d1a35d496daa055bdaaf4a775482438ac49bf
Manabu TERADA <terada@cmscom.jp>
2024-10-21 20:15:27 +0900

    編集名変更と改行を入れた

--- choice_app.py
+++ choice_app.py
@@ -2,9 +2,15 @@ import random
 import streamlit as st
 
 st.title("文字列を選択するアプリ")
-text = st.text_input("スペース区切りで単語を入力してください。")  # 入力ボックス
-splited_text = text.replace("　", " ").split(" ")  # スペース区切りでリスト化
-st.write("入力した単語: ", splited_text)  # リストを表示
-if st.button("一つを選ぶ"):  # ボタンを設置し、ボタンが押されたら実行
-    choice = random.choice(splited_text)  # ランダムに1つだけを選択
+# 入力ボックス
+text = st.text_input("スペース区切りで単語を入力してください。")
+# スペース区切りでリスト化
+words = text.replace("　", " ").split(" ")
+
+# リストを表示
+st.write("入力した単語: ", words)
+
+# ボタンを設置し、ボタンが押されたら実行
+if st.button("一つを選ぶ"):
+    choice = random.choice(words)  # ランダムに1つだけを選択
     st.write("選択された単語: ", choice)  # 選択されたものを表示
